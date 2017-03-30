b4657c38b8ce2b89bda0a9c4e117acaa35c14bd4
Aydar Khubbatullin <aydarhub@gmail.com>
2017-03-30 13:51:02 +0300

    Добавил ToolBar.

--- app/src/main/java/com/aydar/messenger/MainActivity.java
+++ app/src/main/java/com/aydar/messenger/MainActivity.java
@@ -3,11 +3,22 @@ package com.aydar.messenger;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
     }
 }
