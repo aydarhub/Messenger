d797625435ab3c2a641bde42916f4df3aa16ac7b
Aydar Khubbatullin <aydarhub@gmail.com>
2017-03-31 13:41:37 +0300

    Добавил TabLayout.

--- app/src/main/java/com/aydar/messenger/MainActivity.java
+++ app/src/main/java/com/aydar/messenger/MainActivity.java
@@ -3,18 +3,37 @@ package com.aydar.messenger;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends Activity {
+import com.aydar.messenger.adapter.TabsPagerFragmentAdapter;
+
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initToolbar();
+        initTabs();
+
+    }
+
+    private void initTabs() {
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 
     private void initToolbar() {
